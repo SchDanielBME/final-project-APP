9c8ff721fd2281f3392e4d386cc4689762c59811
SchDanielBME <daniel.sch.bme@gmail.com>
2024-06-16 17:17:43 +0300

    visual changes and more

--- DS final project/Assets/Scripts/SelectScene.cs
+++ DS final project/Assets/Scripts/SelectScene.cs
@@ -10,13 +10,20 @@ public class SelectScene : MonoBehaviour
     [SerializeField] private GameObject data;
     [SerializeField] private GameObject plane;
     [SerializeField] private GameObject room;
+    [SerializeField] private GameObject parquet;
+    [SerializeField] private GameObject celling;
     [SerializeField] private GameObject props;
+    [SerializeField] private GameObject pipe;
+
     private int scence;
 
     void Start()
     {
         room.SetActive(false);
         props.SetActive(false);
+        parquet.SetActive(false);
+        celling.SetActive(false);
+        pipe.SetActive(false);
         Data currentScene = data.GetComponent<Data>();
         currentScene.OnCurentScenes += ComponentCurentScenes;
     }
@@ -26,26 +33,46 @@ public class SelectScene : MonoBehaviour
         scence = e.CurrentSceneNum;
        
         if (scence == 1) {
-            Debug.Log("OFFICE");
+            Debug.Log("S0");
             room.SetActive(true);
-            props.SetActive(true);
+            props.SetActive(false);
+            parquet.SetActive(true);
+            celling.SetActive(true);
+            pipe.SetActive(false);
             plane.SetActive(false);
         }
 
         if (scence == 2)
         {
-            Debug.Log("room");
+            Debug.Log("S1");
             room.SetActive(true);
-            props.SetActive(false);
+            props.SetActive(true);
+            parquet.SetActive(true);
+            celling.SetActive(true);
+            pipe.SetActive(false);
             plane.SetActive(false);
         }
 
         if (scence == 3)
         {
-            Debug.Log("empty");
+            Debug.Log("R0");
             room.SetActive(false);
             props.SetActive(false);
-            plane.SetActive(true);
+            parquet.SetActive(true);
+            celling.SetActive(true);
+            pipe.SetActive(true);
+            plane.SetActive(false);
+        }
+
+        if (scence == 4)
+        {
+            Debug.Log("R1");
+            room.SetActive(false);
+            props.SetActive(true);
+            parquet.SetActive(true);
+            celling.SetActive(true);
+            pipe.SetActive(true);
+            plane.SetActive(false);
         }
 
     }
